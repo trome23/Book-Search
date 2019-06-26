8c13aaf2e671a5d5c5b775496009e62a42032f32
trome23 <romennoodles23@yahoo.com>
2019-06-26 17:51:26 -0600

    delete route working in books.js

--- routes/api/books.js
+++ routes/api/books.js
@@ -7,13 +7,24 @@ const Book = require('../../models/bookModel');
 router.get('/', (req, res) => {
     Book.find()
     .then(books => res.json(books))
-})
+});
 
 router.post('/', (req, res) => {
     const newBook = new Book({
-        authors: req.body.authors
+        title: req.body.title,
+        authors: req.body.authors,
+        description: req.body.description,
+        image: req.body.image,
+        link: req.body.link
     });
     newBook.save().then(book => res.json(book))
+});
+
+router.delete('/:id', (req, res) => {
+    Book.findById(req.params.id)
+    .then(book => book.remove().then(() => res.json({success: true})))
+    .catch(err=>res.status(404).json({success: false}))
 })
+
 
 module.exports = router;
