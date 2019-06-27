8d5b9cb5cba194fd47a2560e9398d01a2fa3bdd4
trome23 <romennoodles23@yahoo.com>
2019-06-27 17:13:21 -0600

    created client side of file w/ both server and client running concurrently by npm run dev

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser")
 const books = require("./routes/api/books");
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Initialize Express
 const app = express();
